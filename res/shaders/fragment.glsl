d8a1a942390f1eab8b60fdc5f02fe3a8d7ecf946
Kbzinho-66 <gvalles@ucs.br>
2022-11-28 18:35:24 -0300

    Acho que assim é mais fácil pra trabalhar

---
+++ res/shaders/fragment.glsl
@@ -0,0 +1,13 @@
+#version 400
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+// Texturas
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main() {
+//    FragColor = texture(texture1, TexCoord);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
+}
