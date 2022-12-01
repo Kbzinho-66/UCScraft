eaa6777351f0b2f1a7d495faacc538b8d8b4e15a
Kbzinho-66 <gvalles@ucs.br>
2022-11-30 22:34:37 -0300

    Temos um chão impenetrável

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Um joguinho de blocos feito por dois caras, ao invés de um
 ## TO-DO
 
 - [x] Configurar o Skybox
-- [ ] Criar um chão
-- [ ] Não deixar a câmera atravessar o chão (ou o Skybox, quem sabe)
+- [x] Criar um chão
+- [x] Não deixar a câmera atravessar o chão (ou o Skybox, quem sabe)
 - [ ] Descobrir como capturar/calcular o lugar onde é pra posicionar um bloco
 - [ ] Encontrar a posição no tabuleiro mais próxima
 - [ ] Criar uma estrutura de dados pra saber se essa posição tá livre ou não
--- main.cpp
+++ main.cpp
@@ -9,14 +9,23 @@
 #include "camera.h"
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
-void mouseCallback(GLFWwindow *window, double xpos, double ypos);
 
-// settings
+void processInput(GLFWwindow* window);
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+
+// Configurações da tela
 const unsigned int WIDTH = 1080;
 const unsigned int HEIGHT = 720;
 
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+// Constantes de texturas
+#define SKY 0
+#define GROUND 1
+#define CONCRETE 2
+#define ICE 3
+#define WOOD 4
+
+Camera camera(glm::vec3(0.0f, 1.0f, 3.0f));
 float lastX = WIDTH / 2.0f;
 float lastY = HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -46,7 +55,7 @@ int main() {
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // Inicializar o GLEW
-    if(glewInit() != GLEW_OK) {
+    if (glewInit() != GLEW_OK) {
         std::cout << "Ocorreu um erro iniciando o GLEW!" << std::endl;
     } else {
         std::cout << "GLEW OK!" << std::endl;
@@ -57,10 +66,10 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     // Preparar o shader
-    Shader ourShader("../res/shaders/vertex.glsl", "../res/shaders/fragment.glsl");
+    Shader shader("../res/shaders/vertex.glsl", "../res/shaders/fragment.glsl");
 
     // Conjuntos de vértices
-    GLfloat boxVertices[] = {
+    GLfloat blockVertices[] = {
             -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
             0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
             0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
@@ -102,16 +111,6 @@ int main() {
             0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
             -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
             -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-
-    };
-
-    GLfloat groundVertices[] = {
-            -100.0f, -0.6f, -100.0f, 0.0f, 1.0f,
-            100.0f, -0.6f, -100.0f, 1.0f, 1.0f,
-            100.0f, -0.6f, 100.0f, 1.0f, 0.0f,
-            100.0f, -0.6f, 100.0f, 1.0f, 0.0f,
-            -100.0f, -0.6f, 100.0f, 0.0f, 0.0f,
-            -100.0f, -0.6f, -100.0f, 0.0f, 1.0f,
     };
 
     // Criar o modelo de um bloco
@@ -122,102 +121,51 @@ int main() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(boxVertices), boxVertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof( blockVertices ), blockVertices, GL_STATIC_DRAW);
 
-    // Definir a posição do bloco
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) nullptr);
+    // Definir coordenadas dos vértices
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) nullptr);
     glEnableVertexAttribArray(0);
-    // Definir a posição da textura
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) (3 * sizeof(float)));
+    // Definir coordenadas da textura
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) ( 3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
 
     // Carregar as texturas
-//    GLuint* textures = new GLuint[5];
-    // TODO Reduzir esse repeteco
-    GLuint skyTexture, groundTexture, iceTexture;
-    // Textura 1 (Skybox)
-    // ---------
-    glGenTextures(1, &skyTexture);
-    glBindTexture(GL_TEXTURE_2D, skyTexture);
-    // Configurações da textura
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    // Carregar a imagem e criar uma textura a partir dela
-    int texWidth, texHeight, nrChannels;
-    std::string texName = "../res/textures/Sky.jpg";
-    // Inverter a imagem quando ler
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char *data = stbi_load(texName.c_str(), &texWidth, &texHeight, &nrChannels, 0);
-    if (data) {
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "Erro ao carregar a textura " << texName << std::endl;
-    }
-    stbi_image_free(data);
-
-    // Textura 2 (Chão)
-    // ---------
-    glGenTextures(1, &groundTexture);
-    glBindTexture(GL_TEXTURE_2D, groundTexture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    texName = "../res/textures/Ground.jpg";
-    data = stbi_load(texName.c_str(), &texWidth, &texHeight, &nrChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "Erro ao carregar a textura " << texName << std::endl;
-    }
-    stbi_image_free(data);
-
-    // Textura 3 (Bloco de gelo)
-    // ---------
-    glGenTextures(1, &iceTexture);
-    glBindTexture(GL_TEXTURE_2D, iceTexture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    texName = "../res/textures/Ice.jpg";
-    data = stbi_load(texName.c_str(), &texWidth, &texHeight, &nrChannels, 0);
-    if (data) {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cout << "Erro ao carregar a textura " << texName << std::endl;
-    }
-    stbi_image_free(data);
-
-    // Passar pro shader em que unidade cada textura tá
-    ourShader.use();
-    ourShader.setInt("skyTexture", 0);
-    ourShader.setInt("iceTexture", 1);
-
-    glm::vec3 cubePositions[] = {
-            glm::vec3(0.0f, 0.0f, 0.0f),
-            glm::vec3(2.0f, 5.0f, -15.0f),
-            glm::vec3(-1.5f, -2.2f, -2.5f),
-            glm::vec3(-3.8f, -2.0f, -12.3f),
-            glm::vec3(2.4f, -0.4f, -3.5f),
-            glm::vec3(-1.7f, 3.0f, -7.5f),
-            glm::vec3(1.3f, -2.0f, -2.5f),
-            glm::vec3(1.5f, 2.0f, -2.5f),
-        glm::vec3( 1.5f,  0.2f, -1.5f),
-        glm::vec3(-1.3f,  1.0f, -1.5f)
+    auto* textures = new GLuint[5];
+    std::string textureFiles[5] = {
+            "Sky.jpg",
+            "Ground.jpg",
+            "Concrete.jpg",
+            "Ice.jpg",
+            "Wood.jpg"
     };
+
+    glGenTextures(5, textures);
+    for (int i = 0; i < 5; ++i) {
+        glBindTexture(GL_TEXTURE_2D, textures[i]);
+
+        // Configurações da textura
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // Carregar a imagem e criar uma textura a partir dela
+        int texWidth, texHeight, nrChannels;
+        std::string texName = "../res/textures/" + textureFiles[i];
+        // Inverter a imagem quando ler
+        stbi_set_flip_vertically_on_load(true);
+        unsigned char* data = stbi_load(texName.c_str(), &texWidth, &texHeight, &nrChannels, 0);
+        if (data) {
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texWidth, texHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        } else {
+            std::cout << "Erro ao carregar a textura " << texName << std::endl;
+        }
+        stbi_image_free(data);
+    }
 
     // Loop principal de renderização
     while (!glfwWindowShouldClose(window)) {
@@ -233,74 +181,56 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Ativar o shader de fato
-        ourShader.use();
+        shader.use();
 
-        /*************************** Desenhar o Skybox ***************************/
-        // Carregar a textura do céu
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, skyTexture);
-
-        // Inicializar as matrizes de transformação
-        glm::mat4 model = glm::mat4(1.0f);
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
-
-        // Como é o Skybox, deixar enorme e girando quase imperceptivelmente
-        model = glm::scale(model, glm::vec3(100.0f));
-        model = glm::rotate(model, (float) (glfwGetTime() / 500.0), glm::vec3(0.0f, -0.5f, 0.0f));
-
-        view = camera.GetViewMatrix();
-        projection = glm::perspective(glm::radians(camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 200.0f);
-
-        // Passar todas as matrizes pros shaders
-        ourShader.setMat4("model", model);
-        ourShader.setMat4("view", view);
-        ourShader.setMat4("projection", projection);
-
-        // Renderizar o Skybox
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
-        /*************************** Desenhar o chaõ ***************************/
-        // Carregar a textura do céu
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, skyTexture);
-
-        // Inicializar as matrizes de transformação
-        model = glm::mat4(1.0f);
-
-        // O chão pode ter o mesmo tamanho do Skybox
-        model = glm::scale(model, glm::vec3(100.0f));
 
         view = camera.GetViewMatrix();
         projection = glm::perspective(glm::radians(camera.Zoom), (float) WIDTH / (float) HEIGHT, 0.1f, 200.0f);
 
-        // Passar todas as matrizes pros shaders
-        ourShader.setMat4("model", model);
-        ourShader.setMat4("view", view);
-        ourShader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        shader.setMat4("projection", projection);
 
-        // Renderizar o Skybox
-        glBindVertexArray(VAO);
+        /*************************** Desenhar o Skybox ***************************/
+        // Carregar a textura do céu
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[SKY]);
+        // Inicializar o modelo do Skybox
+        glm::mat4 model = glm::mat4(1.0f);
+        // Deixar enorme e girando quase imperceptivelmente
+        model = glm::scale(model, glm::vec3(100.0f));
+        model = glm::rotate(model, (float) ( glfwGetTime() / 500.0 ), glm::vec3(0.0f, -0.5f, 0.0f));
+        // Passar pro shader
+        shader.setMat4("model", model);
+        // Renderizar
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        /*************************** Desenhar o chão ***************************/
+        // Carregar a textura
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textures[GROUND]);
+        // Inicializar o modelo
+        model = glm::mat4(1.0f);
+        // Deslocar o chão pra alinhar com a face inferior dos blocos em y = 0
+        model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
+        // O chão pode ter a mesma área do Skybox
+        model = glm::scale(model, glm::vec3(100.0f, 0.01f, 100.0f));
+        shader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         // Desenhar os blocos do jogador
         glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, iceTexture);
+        glBindTexture(GL_TEXTURE_2D, textures[ICE]);
 
-        for (unsigned int i = 0; i < 1; i++) {
-            model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            model = glm::scale(model, glm::vec3(0.5f));
-            float angle = 20.0f * (i + 1);
-//            model = glm::rotate(model, (angle * (float) glfwGetTime()) / 20, glm::vec3(1.0f, 0.3f, 0.5f));
-            //model = glm::rotate(model, glm::radians(angle) + glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
+//        for (unsigned int i = 0; i < 1; i++) {
+        model = glm::mat4(1.0f);
+        // model = glm::rotate(model, glm::radians(angle) + glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+        shader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+//        }
 
         // Mostrar as mudanças na tela
         glfwSwapBuffers(window);
@@ -310,6 +240,7 @@ int main() {
     // Liberar todos os recursos usados
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    delete[] textures;
 
     glfwTerminate();
     return 0;
